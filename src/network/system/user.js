ebef94e46cbf6844ff497b117e230ec2449a479a
lifuhao <lifuhao@123.com>
2020-08-13 18:03:21 +0800

    增加查看页面

--- src/network/system/user.js
+++ src/network/system/user.js
@@ -1,17 +1,27 @@
 import {request} from "../request";
 
-export function search(page,size,status,loginName) {
+//查询用户列表
+export function search(params) {
   return request({
     url: '/user/search',
+    params: params
+  })
+}
+
+//根据用户Id删除对象
+export function deleteById(id) {
+  return request({
+    url: '/user/delete',
     params: {
-      page,size,status,loginName
+      id
     }
   })
 }
 
-export function deleteById(id) {
+//根据用户Id查询对象
+export function findById(id) {
   return request({
-    url: '/user/delete',
+    url: '/user/findById',
     params: {
       id
     }
--- src/views/system/user/User.vue
+++ src/views/system/user/User.vue
@@ -2,20 +2,20 @@
   <div style="background-color: #ffffff;height: 100%;width: 100%;">
     <div style="padding: 10px;">
       <div style="padding-bottom: 30px;padding-top: 10px;">
-        <el-row :gutter="0">
-          <el-col :span="2"><div style="font-size:12px;color: #a3a3a5;padding-top: 5px;padding-left: 10px;">用户名:</div></el-col>
-          <el-col :span="4"><el-input v-model="loginName" placeholder="请输入用户名"></el-input></el-col>
-          <el-col :span="2"><div style="font-size:12px;color: #a3a3a5;padding-top: 5px;padding-left: 20px;">状态:</div></el-col>
+        <el-row :gutter="10">
+          <el-col :span="1"><div class="label">用户名:</div></el-col>
+          <el-col :span="4"><el-input v-model="params.loginName" placeholder="请输入用户名"></el-input></el-col>
+          <el-col :span="1"><div class="label">状态:</div></el-col>
           <el-col :span="4">
-            <el-select v-model="status" placeholder="请选择">
+            <el-select v-model="params.status" placeholder="请选择">
               <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
             </el-select>
           </el-col>
-          <el-col :span="2"><div style="font-size:12px;color: #a3a3a5;padding-top: 5px;padding-left: 45px;">创建日期:</div></el-col>
-          <el-col :span="6">
+          <el-col :span="1"><div class="label">创建:</div></el-col>
+          <el-col :span="7">
             <div class="block">
               <el-date-picker
-                      v-model="date"
+                      v-model="params.dates"
                       type="daterange"
                       range-separator="至"
                       start-placeholder="开始日期"
@@ -23,27 +23,35 @@
               </el-date-picker>
             </div>
           </el-col>
-          <el-col :span="4">
-            <el-button type="primary" @click="searchUserList">查 询</el-button>
+          <el-col :span="2">
+            <div style="text-align: right">
+              <el-button type="primary" size="mini" class="el-icon-search" @click="searchUserList">查询</el-button>
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <div style="text-align: left">
+              <el-button type="warning" size="mini" class="el-icon-refresh-left" @click="resetWhere">重置</el-button>
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <div style="text-align: right">
+              <el-button type="success" size="mini" class="el-icon-circle-plus-outline" @click="searchUserList">添加</el-button>
+            </div>
           </el-col>
         </el-row>
       </div>
-      <div style="padding-bottom: 10px;margin-left: 10px;">
+      <div style="float: left;font-size: 16px;color: #222222;font-weight: bold;">
         <h5>用户列表</h5>
       </div>
       <el-table :data="parseTableData" size="mini" border stripe style="width: 100%" highlight-current-row>
-        <el-table-column prop="name" label="姓名" align="center"/>
-        <el-table-column prop="loginName" label="用户名" align="center"/>
-        <el-table-column prop="email" label="邮箱" align="center"/>
-        <el-table-column prop="phone" label="手机号" align="center"/>
-        <el-table-column prop="seq" label="排序" align="center"/>
-        <el-table-column prop="status" label="状态" align="center"/>
-        <el-table-column prop="describe" label="描述" align="center"/>
-        <el-table-column align="center" label="操作">
+        <el-table-column  v-for="item in table.tableColumn" :prop="item.prop" :label="item.label" :align="item.align"/>
+        <el-table-column align="center"  label="操作">
           <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-            <el-button type="text" size="small">编辑</el-button>
-            <el-button @click="deleteById(scope.row)" type="text" size="small">删除</el-button>
+            <el-row :gutter="0">
+              <el-col :span="8"><i class="el-icon-search" @click="handleClick(scope.row)"></i></el-col>
+              <el-col :span="8"><i class="el-icon-edit"></i></el-col>
+              <el-col :span="8"><i class="el-icon-delete" @click="deleteById(scope.row)"></i></el-col>
+            </el-row>
           </template>
         </el-table-column>
       </el-table>
@@ -51,41 +59,60 @@
         <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
-                :current-page="currentPage"
+                :current-page="params.page"
                 :page-sizes="[10, 15, 20, 25]"
-                :page-size=pageSize
+                :page-size=params.size
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="total">
         </el-pagination>
       </div>
-      <el-dialog
-              title="详情"
-              :visible.sync="centerDialogVisible"
-              :center="true"
-              style="align-content: center">
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="centerDialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
-        </span>
-      </el-dialog>
+      <!--查看详情弹出组件-->
+      <UserDetail :isDetailDialog="isDetailDialog" :detailObj="detailObj"></UserDetail>
     </div>
   </div>
 </template>
 
 <script>
 
-  import {search,deleteById} from "network/system/user";
+  import {search,deleteById,findById} from "network/system/user";
+  import UserDetail from "./UserDetail";
 
   export default {
+    components: {
+      UserDetail
+    },
     methods: {
+      //加载列表
+      search(params){
+        search(params).then(res => {
+          this.table.tableData = res.data.data;
+          this.table.total = res.data.count;
+        })
+      },
+      //查询
+      searchUserList(){
+        this.packParams();
+        this.search(this.params);
+      },
+      //重置
+      resetWhere(){
+        this.params.startDate = null;
+        this.params.endDate = null;
+        this.params.loginName = '';
+        this.params.status = null;
+        this.params.dates = '';
+      },
       //查看
       handleClick(row) {
-        this.centerDialogVisible = true;
+        findById(row.id).then( res => {
+          this.detailObj =  res.data
+        });
+        this.isDetailDialog = true;
       },
       //删除
       deleteById(row) {
         deleteById(row.id).then(res => {
-          this.searchUser(this.currentPage,this.pageSize,this.status,this.loginName);
+          this.search(this.currentPage,this.pageSize,this.status,this.loginName);
           if(res.data.success){
             this.$message.success(res.data.message);
           }else {
@@ -93,52 +120,92 @@
           }
         })
       },
-      //加载列表
-      searchUser(currentPage,pageSize,status,loginName){
-        search(currentPage,pageSize,status,loginName).then(res => {
-          this.tableData = res.data.data;
-          this.total = res.data.count;
-        })
-      },
-      //条件查询
-      searchUserList(){
-        this.searchUser(this.currentPage,this.pageSize,this.status,this.loginName);
-      },
-      //分页change时间
+      //分页change事件
       handleSizeChange(val) {
-        this.searchUser(this.currentPage,val,this.status,this.loginName);
-
+        this.packParams();
+        this.params.size = val;
+        this.search(this.params);
       },
-      //分页change时间
+      //分页change事件
       handleCurrentChange(val) {
-        this.searchUser(val,this.pageSize,this.status,this.loginName);
+        this.packParams();
+        this.params.page = val;
+        this.search(this.params);
+      },
+      //封装查询条件
+      packParams() {
+        const arr = this.params.dates.split(",");
+        this.params.startDate = arr[0],
+        this.params.endDate = arr[1]
       }
     },
+    //vue实例加载完成后调用列表查询
     created() {
-      this.searchUser(this.currentPage,this.pageSize,this.status,this.loginName);
+      this.search(this.params);
     },
     computed: {
+      //处理列表显示内容
       parseTableData() {
-        for(let data of this.tableData){
-          data.status=data.status==1?'可用':'不可用';
+        for(let data of this.table.tableData){
+          data.status=data.status == 1?'可用':'不可用';
         }
-        return this.tableData;
+        return this.table.tableData;
       }
     },
     data() {
       return {
-        options: [{value: '1',label: '可用'}, {value: '2',label: '不可用'}],
-        tableData: [],
-        total: null,
-        centerDialogVisible: false,
-        currentPage:1,
-        pageSize:10,
-        status: null,
-        loginName: "",
-        date: ""
+        params: {//查询参数
+          page:1,
+          size:10,
+          loginName: '',
+          status: null,
+          dates: '',
+          startDate: null,
+          endDate: null
+        },
+        table: {//列表参数
+          tableData: [],
+          total: null,
+          tableColumn: column
+        },
+        options: [{value: '1',label: '可用'}, {value: '2',label: '不可用'}],//下来框选项
+        isDetailDialog: false,//是否打开详情页面弹出框
+        detailObj: {}//给详情弹出框传入要展示的对象
       }
     }
   }
+
+  const column = [
+    {
+      prop: 'name',
+      label: '姓名',
+      align: 'center'
+    },{
+      prop: 'loginName',
+      label: '用户名',
+      align: 'center'
+    },{
+      prop: 'email',
+      label: '邮箱',
+      align: 'center'
+    },{
+      prop: 'phone',
+      label: '手机号',
+      align: 'center'
+    },{
+      prop: 'seq',
+      label: '排序',
+      align: 'center'
+    },{
+      prop: 'status',
+      label: '状态',
+      align: 'center'
+    },{
+      prop: 'describe',
+      label: '描述',
+      align: 'center'
+    }
+  ]
 </script>
 
 <style>
@@ -146,4 +213,13 @@
     padding: 0px;
     overflow-y: auto;
   }
+
+  .label{
+    text-align: right;
+    font-size: 12px;
+    font-weight: bold;
+    margin-top: 5px;
+    color: #222222;
+  }
+
 </style>
